ed025eb96a91c4e0df3c4e8b3ed0ab4ddb2ab399
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2018-04-13 09:49:23 +0200

    main

---
+++ src/main/java/com/mycompany/controlcod/EjemploJsoup.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.controlcod;
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class EjemploJsoup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+    Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
+    
+    String title = doc.title();
+    System.out.println("title:"+title);   
+    
+    
+    Elements links = doc.select("a");
+      for (Element link : links) {
+               
+        System.out.println("text: " + link.text());
+
+
+    
+    
+    }
+}
+}
+
